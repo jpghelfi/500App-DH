e7874aa0285eef84c9633255d15f4280df69afe6
jpghelfi <juan.ghelfi@tictapps.com>
2017-06-21 00:12:28 -0300

    no message

---
+++ 500App/CommnetsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommnetsTableViewCell.swift
+//  500App
+//
+//  Created by Juan Pablo Ghelfi on 6/20/17.
+//  Copyright © 2017 Digital House. All rights reserved.
+//
+
+import UIKit
+
+class CommnetsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
